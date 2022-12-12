956223f3499bf09cc385d077fbe5d8bed5afd826
sergejsvoronins <svoronins@gmail.com>
2022-12-12 11:41:31 +0100

    Local storage functions are done

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,7 +8,23 @@ createProduct("Thin Case","","Apple","blå","349",0);
 createProduct("Thin Case","","Samsung","röd","349",0);
 createProduct("Thin Case","","Samsung","grön","349",0);
 createProduct("Thin Case","","Samsung","blå","349",0);
-console.log(productList);
+
+
+loadToLocalStorage(productList);
+loadFromloaclStorage();
+
+
+
+function loadToLocalStorage (someList: Product []){
+    localStorage.setItem ("productList", JSON.stringify(someList));
+}
+
+function loadFromloaclStorage () {
+    let products : [] = JSON.parse(localStorage.getItem("productList") || "[]");
+    let productList = products.map (( products:Product )=>{
+        return new Product (products.title,products.url, products.brand, products.color, products.price, products.buyAmount)
+    })
+}
 
 
 function createProduct (
@@ -16,29 +32,4 @@ function createProduct (
 ) {
     let product : Product = new Product (title,url,brand,color,price,amount);
     productList.push(product);
-}
-createCartHtml(productList);
-let cartProductsCont = document.getElementById("cartProductsCont") as HTMLDivElement;
-function createCartHtml (someList:Product []) {
-    for (let i=0; i<someList.length;i++){
-        let productImgDiv : HTMLDivElement = document.createElement("div");
-        let productInfoDiv : HTMLDivElement = document.createElement("div");
-        cartProductsCont.appendChild(productImgDiv);
-        cartProductsCont.appendChild(productInfoDiv);
-        let productImg : HTMLImageElement = document.createElement("img");
-        productImgDiv.appendChild(productImg);
-        productImg.src = someList[i].url;
-        let productTitle : HTMLParagraphElement = document.createElement("p");
-        let productPrice : HTMLParagraphElement = document.createElement("p");
-        let productAmountDiv : HTMLDivElement = document.createElement("div");
-        productInfoDiv.appendChild(productTitle);
-        productInfoDiv.appendChild(productPrice);
-        productInfoDiv.appendChild(productAmountDiv);
-        let increaseBtn : HTMLButtonElement = document.createElement("button");
-        increaseBtn.innerHTML = "+";
-        let productAmountNumber : HTMLDivElement = document.createElement ("div");
-        productAmountNumber.innerHTML = (someList[i].buyAmount).toString();
-        let decreaseBtn : HTMLButtonElement = document.createElement("button");
-        decreaseBtn.innerHTML = "-";
-    }
 }
