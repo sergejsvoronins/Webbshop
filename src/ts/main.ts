39387864079101214c3bb283397c3cd5da30765f
sergejsvoronins <svoronins@gmail.com>
2022-12-12 10:11:03 +0100

    class for products is created

---
+++ src/ts/main.ts
@@ -0,0 +1,31 @@
+import { Product } from "./models/product";
+
+let productList : Product [] = [];
+
+// let product : Product = new Product (
+//     "Thin Case",
+//     "",
+//     "Apple",
+//     "röd",
+//     "349",
+//     0
+// );
+// productList.push(product);
+createProduct("Thin Case","","Apple","röd","349",0);
+createProduct("Thin Case","","Apple","grön","349",0);
+createProduct("Thin Case","","Apple","blå","349",0);
+createProduct("Thin Case","","Samsung","röd","349",0);
+createProduct("Thin Case","","Samsung","grön","349",0);
+createProduct("Thin Case","","Samsung","blå","349",0);
+console.log(productList);
+
+
+function createProduct (
+    title:string,url:string,brand:string,color:string, price:string,amount:number
+) {
+    let product : Product = new Product (title,url,brand,color,price,amount);
+    productList.push(product);
+}
+
+
+
---
+++ src/ts/models/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    constructor (
+        public title: string,
+        public url: string,
+        public brand: string,
+        public color: string,
+        public price: string,
+        public buyAmount:number,
+    ){}
+}
