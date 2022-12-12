a5a827d0a300b9924f17ab1a48164239f37bfb21
sergejsvoronins <svoronins@gmail.com>
2022-12-12 11:01:34 +0100

    start with ts to createHtml for cart

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -17,6 +17,28 @@ function createProduct (
     let product : Product = new Product (title,url,brand,color,price,amount);
     productList.push(product);
 }
-
-
-
+createCartHtml(productList);
+let cartProductsCont = document.getElementById("cartProductsCont") as HTMLDivElement;
+function createCartHtml (someList:Product []) {
+    for (let i=0; i<someList.length;i++){
+        let productImgDiv : HTMLDivElement = document.createElement("div");
+        let productInfoDiv : HTMLDivElement = document.createElement("div");
+        cartProductsCont.appendChild(productImgDiv);
+        cartProductsCont.appendChild(productInfoDiv);
+        let productImg : HTMLImageElement = document.createElement("img");
+        productImgDiv.appendChild(productImg);
+        productImg.src = someList[i].url;
+        let productTitle : HTMLParagraphElement = document.createElement("p");
+        let productPrice : HTMLParagraphElement = document.createElement("p");
+        let productAmountDiv : HTMLDivElement = document.createElement("div");
+        productInfoDiv.appendChild(productTitle);
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productAmountDiv);
+        let increaseBtn : HTMLButtonElement = document.createElement("button");
+        increaseBtn.innerHTML = "+";
+        let productAmountNumber : HTMLDivElement = document.createElement ("div");
+        productAmountNumber.innerHTML = (someList[i].buyAmount).toString();
+        let decreaseBtn : HTMLButtonElement = document.createElement("button");
+        decreaseBtn.innerHTML = "-";
+    }
+}
