167ca3494b2ef9f173df955441c92dadc975561f
Emil <emil.salmi@hotmail.se>
2022-12-13 09:25:36 +0100

    redo ts file

--- src/ts/productinfo.ts
+++ src/ts/productinfo.ts
@@ -1,11 +1,4 @@
-import { Product } from "./models/product";
 
-function handleProductInfo{
-    
-    let productContainer : HTMLDivElement = document.getElementById
-    ("hero-product") as HTMLDivElement;
-
-};
 
 
 
